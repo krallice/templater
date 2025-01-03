b442213d30c125d2d96183708c3d98cefd3c002f
krallice <sroz9960@gmail.com>
2025-01-03 21:51:59 +1100

    feat(hlm): Support light/medium pull customisations

--- hlm.py
+++ hlm.py
@@ -4,14 +4,23 @@ from libhlm import HLMStandardGenerator, HLMAlternatePressingGenerator
 
 if __name__ == '__main__':
     hlm = HLMAlternatePressingGenerator(
-        squat=140, 
-        pull=160, 
+        # Squat:
+        squat=140,
+
+        # Press:
         primary_press_name="OHP",
         primary_press=75,
         secondary_press_name="Weighted Dips",
         secondary_press=50,
+
+        # Pull:
+        pull=160,
+
+        # Core Reductions:
         medium_reduction=0.10, 
         light_reduction=0.20,
+        
+        # Header Text:
         header_text="""
 Accessories:
   2-3x10 Chins
